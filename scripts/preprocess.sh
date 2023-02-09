c40a5deb6af5e937c5fb78cfbff62ba779fc187b
Jason Huang <jason5528460@gmail.com>
2023-02-09 18:44:10 +0800

    Add scripts/ folder and move preprocessing script

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -14,6 +14,7 @@ if [[ $# -ne 5 ]]; then
 fi
 
 SCRIPT_DIR=$(dirname "$0")
+PROJECT_DIR=$(realpath "${SCRIPT_DIR}/../")
 root_dir=$(realpath "$1")
 split="$2"
 data_dir=$(realpath "$3")
@@ -52,7 +53,7 @@ fi
 
 # Generate tsv meta files
 if [[ ! -d "${result_dir}/tsv" ]]; then
-    python3 "${SCRIPT_DIR}/gen_tsv.py" "${data_dir}/LibriSpeech" "${result_dir}/tsv" "$split" --speaker_sample_ratio=1 --data_sample_ratio=1
+    python3 "${PROJECT_DIR}/gen_tsv.py" "${data_dir}/LibriSpeech" "${result_dir}/tsv" "$split" --speaker_sample_ratio=1 --data_sample_ratio=1
 else
     echo "Meta files found in ${result_dir}/tsv, skipping generation process."
 fi
@@ -60,23 +61,23 @@ fi
 if [[ $phase == "1" ]]; then
     # Generate MFCC features
     if [[ ! -d "${result_dir}/mfcc" ]]; then
-        python3 "${SCRIPT_DIR}/simple_kmeans/dump_mfcc_feature.py" "${result_dir}/tsv" train 1 0 "${result_dir}/mfcc"
-        python3 "${SCRIPT_DIR}/simple_kmeans/dump_mfcc_feature.py" "${result_dir}/tsv" valid 1 0 "${result_dir}/mfcc"
+        python3 "${PROJECT_DIR}/simple_kmeans/dump_mfcc_feature.py" "${result_dir}/tsv" train 1 0 "${result_dir}/mfcc"
+        python3 "${PROJECT_DIR}/simple_kmeans/dump_mfcc_feature.py" "${result_dir}/tsv" valid 1 0 "${result_dir}/mfcc"
     else
         echo "MFCC features found in ${result_dir}/mfcc, skipping generation process."
     fi
 
     # Train K-means model
     if [[ ! -f "${result_dir}/phase1_kmeans.pt" ]]; then
-        python3 "${SCRIPT_DIR}/simple_kmeans/learn_kmeans.py" "${result_dir}/mfcc" train 1 "${result_dir}/phase1_kmeans.pt" 100 --percent -1 
+        python3 "${PROJECT_DIR}/simple_kmeans/learn_kmeans.py" "${result_dir}/mfcc" train 1 "${result_dir}/phase1_kmeans.pt" 100 --percent -1 
     else
         echo "K-means model checkpoint already exists, skipping training process."
     fi
 
     # Apply K-means model
     if [[ ! -d "${result_dir}/phase1_labels" ]]; then
-        python3 "${SCRIPT_DIR}/simple_kmeans/dump_km_label.py" "${result_dir}/mfcc" train "${result_dir}/phase1_kmeans.pt" 1 0 "${result_dir}/phase1_labels"
-        python3 "${SCRIPT_DIR}/simple_kmeans/dump_km_label.py" "${result_dir}/mfcc" valid "${result_dir}/phase1_kmeans.pt" 1 0 "${result_dir}/phase1_labels"
+        python3 "${PROJECT_DIR}/simple_kmeans/dump_km_label.py" "${result_dir}/mfcc" train "${result_dir}/phase1_kmeans.pt" 1 0 "${result_dir}/phase1_labels"
+        python3 "${PROJECT_DIR}/simple_kmeans/dump_km_label.py" "${result_dir}/mfcc" valid "${result_dir}/phase1_kmeans.pt" 1 0 "${result_dir}/phase1_labels"
     else
         echo "K-means labels found in ${result_dir}/phase1_labels, skipping generation process."
     fi
@@ -93,23 +94,23 @@ elif [[ $phase == "2" ]]; then
     # Generate HuBERT features
     if [[ ! -d "${result_dir}/hubert" ]]; then
         read -p "Please enter Phase-1 HuBERT training checkpoint path: " ckpt_path
-        python3 "${SCRIPT_DIR}/simple_kmeans/dump_hubert_feature.py" "${result_dir}/tsv" train "$ckpt_path" 0 1 0 "${result_dir}/hubert"
-        python3 "${SCRIPT_DIR}/simple_kmeans/dump_hubert_feature.py" "${result_dir}/tsv" valid "$ckpt_path" 0 1 0 "${result_dir}/hubert"
+        python3 "${PROJECT_DIR}/simple_kmeans/dump_hubert_feature.py" "${result_dir}/tsv" train "$ckpt_path" 0 1 0 "${result_dir}/hubert"
+        python3 "${PROJECT_DIR}/simple_kmeans/dump_hubert_feature.py" "${result_dir}/tsv" valid "$ckpt_path" 0 1 0 "${result_dir}/hubert"
     else
         echo "HuBERT features found in ${result_dir}/hubert, skipping generation process."
     fi
 
     # Train K-means model
     if [[ ! -f "${result_dir}/phase2_kmeans.pt" ]]; then
-        python3 "${SCRIPT_DIR}/simple_kmeans/learn_kmeans.py" "${result_dir}/hubert" train 1 "${result_dir}/phase2_kmeans.pt" 500 --percent -1 
+        python3 "${PROJECT_DIR}/simple_kmeans/learn_kmeans.py" "${result_dir}/hubert" train 1 "${result_dir}/phase2_kmeans.pt" 500 --percent -1 
     else
         echo "K-means model checkpoint already exists, skipping training process."
     fi
 
     # Apply K-means model
     if [[ ! -d "${result_dir}/phase2_labels" ]]; then
-        python3 "${SCRIPT_DIR}/simple_kmeans/dump_km_label.py" "${result_dir}/hubert" train "${result_dir}/phase2_kmeans.pt" 1 0 "${result_dir}/phase2_labels"
-        python3 "${SCRIPT_DIR}/simple_kmeans/dump_km_label.py" "${result_dir}/hubert" valid "${result_dir}/phase2_kmeans.pt" 1 0 "${result_dir}/phase2_labels"
+        python3 "${PROJECT_DIR}/simple_kmeans/dump_km_label.py" "${result_dir}/hubert" train "${result_dir}/phase2_kmeans.pt" 1 0 "${result_dir}/phase2_labels"
+        python3 "${PROJECT_DIR}/simple_kmeans/dump_km_label.py" "${result_dir}/hubert" valid "${result_dir}/phase2_kmeans.pt" 1 0 "${result_dir}/phase2_labels"
     else
         echo "K-means labels found in ${result_dir}/phase1_labels, skipping generation process."
     fi
