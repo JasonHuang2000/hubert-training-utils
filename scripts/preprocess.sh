bba979f89762536f0c896263bcd5a12e62855147
Jason Huang <jason5528460@gmail.com>
2023-02-12 20:22:54 +0800

    Add kmeans_ratio and train-960h support

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,15 +1,16 @@
 #/bin/bash
 
 # Show script usage
-if [[ $# -ne 5 ]]; then
+if [[ $# -ne 6 ]]; then
     echo "Use this script to automate pre-processing of HuBERT training on LibriSpeech dataset."
     echo ""
-    echo "Usage: ./preprocess.sh [root_dir] [split] [data_dir] [result_dir] [phase]"
+    echo "Usage: ./preprocess.sh [root_dir] [split] [data_dir] [result_dir] [phase] [kmeans_ratio]"
     echo "  - root_dir: Path to root of LibriSpeech dataset."
-    echo "  - split: The dataset split used for training. Possible values: 100, 360, 500."
+    echo "  - split: The dataset split used for training. Possible values: 100, 360, 500, 960."
     echo "  - data_dir: The target directory for unpacking dataset."
     echo "  - result_dir: The target directory for generated meta files and labels."
     echo "  - phase: The upcoming training phase. Possible values: 1, 2."
+    echo "  - kmeans_ratio: The ratio of training data used in K-means training."
     exit 1
 fi
 
@@ -20,56 +21,65 @@ split="$2"
 data_dir=$(realpath "$3")
 result_dir=$(realpath "$4")
 phase="$5"
+kmeans_ratio="$6"
 data_split_name=""
 
-if [[ $split == "100" ]]; then
-    data_split_name="train-clean-100"
-elif [[ $split == "360" ]]; then
-    data_split_name="train-clean-360"
-elif [[ $split == "500" ]]; then
-    data_split_name="train-other-500"
-else
-    echo "Unsupported dataset split: $split"
-    exit 1
-fi
 
 if [[ ! -d "$data_dir" ]]; then
     mkdir -p "$data_dir"
 fi
 
 # Unpack dataset split
-if [[ ! -d "${data_dir}/LibriSpeech/${data_split_name}" ]]; then
-    echo "Unpacking LibriSpeech train-${split}h split..."
-    tar -xf "${root_dir}/${data_split_name}.tar.gz" --directory ${data_dir}
+unpack_dataset ()
+{
+    split_name="$1"
+    if [[ ! -d "${data_dir}/LibriSpeech/${split_name}" ]]; then
+        echo "Unpacking LibriSpeech ${split_name} split..."
+        tar -xf "${root_dir}/${split_name}.tar.gz" --directory ${data_dir}
+    else
+        echo "LibriSpeech ${split_name} split already found in ${data_dir}. Skipping unpacking process..."
+    fi
+}
+
+if [[ $split == "100" ]]; then
+    unpack_dataset "train-clean-100"
+elif [[ $split == "360" ]]; then
+    unpack_dataset "train-clean-360"
+elif [[ $split == "500" ]]; then
+    unpack_dataset "train-other-500"
+elif [[ $split == "960" ]]; then
+    unpack_dataset "train-clean-100"
+    unpack_dataset "train-clean-360"
+    unpack_dataset "train-other-500"
+else
+    echo "Unsupported dataset split: $split"
+    exit 1
 fi
-if [[ ! -d "${data_dir}/LibriSpeech/dev-clean" ]]; then
-    echo "Unpacking LibriSpeech dev-clean split..."
-    tar -xf "${root_dir}/dev-clean.tar.gz" --directory ${data_dir}
-fi
+unpack_dataset "dev-clean"
 
 if [[ ! -d "$result_dir" ]]; then
     mkdir -p "$result_dir"
 fi
 
 # Generate tsv meta files
-if [[ ! -d "${result_dir}/tsv" ]]; then
-    python3 "${PROJECT_DIR}/gen_tsv.py" "${data_dir}/LibriSpeech" "${result_dir}/tsv" "$split" --speaker_sample_ratio=1 --data_sample_ratio=1
+if [[ ! -d "${result_dir}/tsv-${split}" ]]; then
+    python3 "${PROJECT_DIR}/gen_tsv.py" "${data_dir}/LibriSpeech" "${result_dir}/tsv-${split}" "$split" --speaker_sample_ratio=1 --data_sample_ratio=1
 else
-    echo "Meta files found in ${result_dir}/tsv, skipping generation process."
+    echo "Meta files found in ${result_dir}/tsv-${split}, skipping generation process."
 fi
 
 if [[ $phase == "1" ]]; then
     # Generate MFCC features
     if [[ ! -d "${result_dir}/mfcc" ]]; then
-        python3 "${PROJECT_DIR}/simple_kmeans/dump_mfcc_feature.py" "${result_dir}/tsv" train 1 0 "${result_dir}/mfcc"
-        python3 "${PROJECT_DIR}/simple_kmeans/dump_mfcc_feature.py" "${result_dir}/tsv" valid 1 0 "${result_dir}/mfcc"
+        python3 "${PROJECT_DIR}/simple_kmeans/dump_mfcc_feature.py" "${result_dir}/tsv-${split}" train 1 0 "${result_dir}/mfcc"
+        python3 "${PROJECT_DIR}/simple_kmeans/dump_mfcc_feature.py" "${result_dir}/tsv-${split}" valid 1 0 "${result_dir}/mfcc"
     else
         echo "MFCC features found in ${result_dir}/mfcc, skipping generation process."
     fi
 
     # Train K-means model
     if [[ ! -f "${result_dir}/phase1_kmeans.pt" ]]; then
-        python3 "${PROJECT_DIR}/simple_kmeans/learn_kmeans.py" "${result_dir}/mfcc" train 1 "${result_dir}/phase1_kmeans.pt" 100 --percent -1 
+        python3 "${PROJECT_DIR}/simple_kmeans/learn_kmeans.py" "${result_dir}/mfcc" train 1 "${result_dir}/phase1_kmeans.pt" 100 --percent "${kmeans_ratio}"
     else
         echo "K-means model checkpoint already exists, skipping training process."
     fi
@@ -93,15 +103,15 @@ elif [[ $phase == "2" ]]; then
     # Generate HuBERT features
     if [[ ! -d "${result_dir}/hubert" ]]; then
         read -p "Please enter Phase-1 HuBERT training checkpoint path: " ckpt_path
-        python3 "${PROJECT_DIR}/simple_kmeans/dump_hubert_feature.py" "${result_dir}/tsv" train "$ckpt_path" 0 1 0 "${result_dir}/hubert"
-        python3 "${PROJECT_DIR}/simple_kmeans/dump_hubert_feature.py" "${result_dir}/tsv" valid "$ckpt_path" 0 1 0 "${result_dir}/hubert"
+        python3 "${PROJECT_DIR}/simple_kmeans/dump_hubert_feature.py" "${result_dir}/tsv-${split}" train "$ckpt_path" 0 1 0 "${result_dir}/hubert"
+        python3 "${PROJECT_DIR}/simple_kmeans/dump_hubert_feature.py" "${result_dir}/tsv-${split}" valid "$ckpt_path" 0 1 0 "${result_dir}/hubert"
     else
         echo "HuBERT features found in ${result_dir}/hubert, skipping generation process."
     fi
 
     # Train K-means model
     if [[ ! -f "${result_dir}/phase2_kmeans.pt" ]]; then
-        python3 "${PROJECT_DIR}/simple_kmeans/learn_kmeans.py" "${result_dir}/hubert" train 1 "${result_dir}/phase2_kmeans.pt" 500 --percent -1 
+        python3 "${PROJECT_DIR}/simple_kmeans/learn_kmeans.py" "${result_dir}/hubert" train 1 "${result_dir}/phase2_kmeans.pt" 500 --percent "${kmeans_ratio}"
     else
         echo "K-means model checkpoint already exists, skipping training process."
     fi
