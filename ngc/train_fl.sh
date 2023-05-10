9adf2295fccb2c1d823f1bec4a79a20e127b11c9
Jason Huang <jason5528460@gmail.com>
2023-05-11 02:17:15 +0800

    Fix typo

--- ngc/train_fl.sh
+++ ngc/train_fl.sh
@@ -14,4 +14,4 @@ phase="$1"
 source /ntu-jason/.venv/bin/activate
 
 bash /ntu-jason/hubert-training-utils/scripts/preprocess.sh /LibriSpeech ${split} /ntu-jason/datasets /workspace/preprocessing-${split} 1 ${kmeas_ratio} || exit 1
-bash /ntu-jason/hubert-training-utils/scripts/hubert_tiny.sh "$phase" /ntu-jason/hubert-training-utils /workspace/preprocessing-${split} "/ntu-jason/exp/${phase}/FL" /ntu-jason /ntu-jason/ssl-hubert/flute || exit 1
+bash /ntu-jason/hubert-training-utils/scripts/hubert_tiny_fl.sh "$phase" /ntu-jason/hubert-training-utils /workspace/preprocessing-${split} "/ntu-jason/exp/${phase}/FL" /ntu-jason /ntu-jason/ssl-hubert/flute || exit 1
