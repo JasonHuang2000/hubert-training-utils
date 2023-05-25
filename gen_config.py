82475fba2970351b14608b33c85e6f4e35e91a31
Jason Huang <jason5528460@gmail.com>
2023-05-26 01:43:41 +0800

    Add required args for SGD optimizer

--- gen_config.py
+++ gen_config.py
@@ -20,7 +20,10 @@ def main(
     config["model"]["label_rate"] = label_rate
     config["distributed_training"]["distributed_world_size"] = 1
     config["common"]["fp16"] = False
-    config["optimizer"] = {"_name": "sgd"}
+    config["optimizer"] = {
+        "_name": "sgd",
+        "lr": "${optimization.lr}",
+    }
 
     with open(output_file, "w") as f:
         yaml.safe_dump(config, f)
